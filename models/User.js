f2d5fe84af9b96d38e388f1d5266c4ab382665a2
John O'Brien <johnobrien.dev@gmail.com>
2024-09-11 12:00:27 -0400

    add verify token generate to user model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, default: '' }, 
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verificationToken: String,
 });
+
+userSchema.methods.generateVerificationToken = function() {
+  const token = bcrypt.genSaltSync(10);
+  this.verificationToken = token;
+  this.save();
+  return token;
+};
 
 const User = mongoose.model('User', userSchema);
 
