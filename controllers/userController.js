c8247849d8b2cabfc5a5b279072666a16de0ec0c
John O'Brien <johnobrien.dev@gmail.com>
2024-09-11 11:44:31 -0400

    add send verification in auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const { sendVerificationEmail } = require('./auth');
 
 // get user data
 exports.getUserData = asyncHandler(async (req, res) => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,28 @@
 const passport = require('passport');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  // Configure your email service here
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
 exports.authenticateJWT = passport.authenticate('jwt', { session: false });
+
+exports.sendVerificationEmail = async (user) => {
+  const verificationToken = user.generateVerificationToken();
+
+  const verificationLink = `http://yourwebsite.com/verify/${verificationToken}`;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: user.email,
+    subject: 'Verify Your Account',
+    html: `Please click <a href="${verificationLink}">here</a> to verify your account.`
+  };
+
+  await transporter.sendMail(mailOptions);
+};
